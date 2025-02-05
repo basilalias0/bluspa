b58c5f10e3e84c2890ee6d99efc9e3f7e9239689
Basil Alias <basilalyas2000@gmail.com>
2025-02-05 20:29:34 +0530

    initial commit & added index.js

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const connectDB = require('./DB/connectDB')
+const errorHandler = require('./middlewares/errorHandler')
+const router  = require('./routes')
+const app = express()
+
+connectDB()
+app.use(express.json())
+
+app.use("api/v1",router)
+
+app.use(errorHandler)
+
+
+app.listen(5000,()=>{
+    console.log('server is running on port 5000')
+})
